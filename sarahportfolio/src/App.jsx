af99e3932ba1958b3e76b2580a02808a53b0e6e1
Evan Watson <evanwatson98@gmail.com>
2022-05-09 22:08:53 -0500

    add react project for portfollio

---
+++ sarahportfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Introduction from "./components/Intro"
+import About from './components/about/About';
+
+const App = () => {
+  return ( 
+    <div>
+      <Introduction/>
+      <About/>
+  </div>
+  );
+};
+
+export default App;
---
+++ sarahportfolio/src/components/Intro.jsx
@@ -0,0 +1,31 @@
+import "./intro.css"
+import Shoe from "../img/sarahStella.png"
+const Introduction = () => {
+    return (
+        <div className="i d-flex row">
+            <div className="i-left col">
+                <div className="i-left-wrap">
+                    <h2 className="i-intro">Hello, my name is</h2>
+                    <h1 className="i-name">Sarah Cafora</h1>
+                    <div className="i-title">
+                        <div className="i-title-wrap">
+                            <div className="i-title-item">Creative Designer</div>
+                            <div className="i-title-item">Entrepreneur</div>
+                            <div className="i-title-item">Videographer</div>
+                            <div className="i-title-item">Model</div>
+                            <div className="i-title-item">Fashion Queen</div>
+                        </div>
+                    </div>
+                    <p className="i-desc">I am young and ambitious woman searching for new ways to express my passions. From designing clothes to writing my blogs, I aim to express my creativity through many mediums.</p>
+                </div>
+                
+            </div>
+            <div className="i-right col">
+                <div className="i-bg"></div>
+                <img src={Shoe} alt="" className="i-img"/>
+            </div>
+        </div>
+    )
+}
+
+export default Introduction;
